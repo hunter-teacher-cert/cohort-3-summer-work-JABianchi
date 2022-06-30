19e0567ea0f22a598ada109aa9e4ab86d3ef7206
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-06-30 13:51:49 +0000

    Nim stub

---
+++ programming/1/Nim.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim{
+
+
+  public static void main(String[] antartica){
+    System.out.println("Nim class starting!");
+
+    int stones = 12;
+    int stonesTaken;
+
+    Scanner eyes = new Scanner(System.in);
+
+    //loop until game ends
+    while(true){
+
+      //prompt user input (user turn)
+
+
+      //calculate numbers of stone remaining
+
+
+      //print
+
+
+      //check for win
+
+
+      //machine turn
+
+      //calculate numbers of stone remaining
+
+
+      //check win
+
+      
+    }
+
+    
+    
+  }
+
+  
+}
