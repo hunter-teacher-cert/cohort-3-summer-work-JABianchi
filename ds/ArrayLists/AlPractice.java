2ce9684d53ad7005458be4040b1ea58f9e9065a1
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-07-12 15:37:09 +0000

    AlPractice 5 methods

--- ds/ArrayLists/AlPractice.java
+++ ds/ArrayLists/AlPractice.java
@@ -1,27 +1,73 @@
+/**
+ * AlPractice
+ * collaborators: Rachel Kaufman, Joel Bianchi, Latoya B
+ */
+
+
+/** TO DO AFTER COMPLETING BASE REQUIREMENTS
+- add in "catch" for swap indexes out of bounds
+- add in check for same length in sumArrays
+*/
+
 import java.io.*;
 import java.util.*;
 
-/** Methods to write
-
+/** 
 Basic level:
 ------------
-public static ArrayList<Integer> buildRandomList(int size, int maxval)
-public static int sumOfList(ArrayList<Integer> dataList)
++ buildRandomList(int size, int maxval)
++ sumOfList(ArrayList<Integer> dataList)
 
 Intermediate level:
 -------------------
-public static void swapElements(ArrayList<Integer> dataList, int indexA,int indexB)
-public static void removeValue(ArrayList<Integer> dataList, int valueToRemove)
++ swapElements(ArrayList<Integer> dataList, int indexA,int indexB)
++ removeValue(ArrayList<Integer> dataList, int valueToRemove)
 
 Challenge level:
 -------------------
-public static ArrayList<Integer> sumLists(ArrayList<Integer>ListA,ArrayList<Integer>ListB)
-public static ArrayList<Integer> zipLists(ArrayList<Integer>ListA,ArrayList<Integer>ListB)
++ sumLists(ArrayList<Integer>ListA,ArrayList<Integer>ListB)
+- zipLists(ArrayList<Integer>ListA,ArrayList<Integer>ListB)
 */
 
 
 public class AlPractice{
 
+
+ public static void main(String[] args) {
+
+    //test buildRandomList
+    ArrayList<Integer> al = buildRandomList(10,15);
+    System.out.println("\nArrayList al:\n" + al);
+
+    //test the sumOfList
+    System.out.println("\nThe sum of ArrayList al is: "
+ + sumOfList(al));
+
+    // test swapElements
+    swapElements(al,2,6);
+    System.out.println("\nArrayList al after swapping the elements at 2 and 6:\n" + al);
+
+    // test removeValue
+    al.add(5);
+    al.add(10);
+    al.add(5);
+    al.add(13);
+    al.set(2,5);
+    al.set(3,5);
+    System.out.println("\nArrayList al after adding 4 more elements & setting indices 2 and 3 to the value 5:\n" + al);
+    removeValue(al,5);
+    System.out.println("\nRemove all 5 values from ArrayList al:\n" + al);
+
+    //test out sumLists
+    ArrayList<Integer> al2 = buildRandomList(10,15);
+    System.out.println("\nArrayList al2:\n" + al2);
+    ArrayList<Integer> al3 = buildRandomList(10,15);
+    System.out.println("\nArrayList al3:\n" + al3);
+    ArrayList<Integer> al4 = sumLists(al2, al3);
+    System.out.println("\nThe sum of the elements of al2 and al3 is ArrayList al4:\n" + al4);
+
+  }
+  
   /**
   Parameters:
   - size - an integer representing how many items to add to the list
@@ -33,8 +79,24 @@ public class AlPractice{
   - A new ArrayList of Integers with each value being a random
     number between 0 and maxval (not including maxval).
   */
+  // buildRandomList generates an ArrayList of length size where each value is a random number up to maxval
   public static ArrayList<Integer> buildRandomList(int size, int maxval){
-    return null;//placeholder to compile.
+
+    //constructing a new ArrayList
+    ArrayList<Integer> randomList = new ArrayList<Integer>(size);
+
+    //traverse list
+    for (int i = 0; i < size; i++) {
+
+      //creating a random object
+      Random rand = new Random();
+      
+      //add a random integer to all indices
+      randomList.add(rand.nextInt(maxval)); 
+    }
+
+    //return the new arraylist created
+    return randomList;
   }
 
   /**
@@ -43,8 +105,21 @@ public class AlPractice{
   Returns:
   - The sum of all of the elements of the ArrayList.
   */
+  // sumOfList returns the sum of all of the elements of the ArrayList dataList.
   public static int sumOfList(ArrayList<Integer> dataList){
-    return 0;//placeholder to compile.
+
+    //initialize a variable for the sum 
+    int listSum = 0;
+
+    //traverse the array
+    for (int i = 0; i < dataList.size(); i++) {
+
+      //add each elements value to the sum
+      listSum += dataList.get(i);
+    }
+    
+    //return the sum
+    return listSum;
   }
 
   /**
@@ -59,8 +134,21 @@ public class AlPractice{
     the value  that was in dataList[index2] is now in dataList[index1].
   - No other values should be modified.
   */
+  // swapElements places the value at index1 into index2 slot, and the value at index2 into the index1 slot.
   public static void swapElements(ArrayList<Integer> dataList, int index1,int index2){
 
+    //create a temp int to hold onto one of the values
+    int placeholder;
+
+    //store the first value
+    placeholder = dataList.get(index1);
+
+    //move the value from the second index to the first slot
+    dataList.set(index1, dataList.get(index2));
+
+    //move the temp value to the second slot
+    dataList.set(index2, placeholder);
+    
   }
 
   /**
@@ -70,8 +158,19 @@ public class AlPractice{
   Postconditions:
   - The dataList is modified such that all occurances of valueToRemove are removed.
   */
+  // removeValue traverses dataList; if any item in the list matches valueToRemove, that item is removed
   public static void removeValue(ArrayList<Integer> dataList, int valueToRemove){
 
+    //for each index in dataList
+    for (int i = 0; i < dataList.size(); i++) {
+
+      //repeatedly check if you're seeing the value we want to remove at the current index
+      while (dataList.get(i) == valueToRemove) {
+
+        //remove it!
+        dataList.remove(i);
+      }
+    }
   }
 
 
@@ -86,8 +185,24 @@ public class AlPractice{
   Postconditions:
   - The parameter ArrayLists should not be modified.
   */
-  public static ArrayList<Integer> sumLists(ArrayList<Integer>ListA,ArrayList<Integer>ListB){
-    return null;//placeholder to compile.
+  // sumLists sums the values at index i from ListA and ListB and adds them to sumArrayList at the same index.
+  public static ArrayList<Integer> sumLists(ArrayList<Integer>ListA, ArrayList<Integer>ListB){
+
+    //determine the size of the new ArrayList
+    //the ArrayList should be the same size as either ListA or ListB
+    int newSize = ListA.size();
+    
+    //create a new ArrayList to hold the sums
+    ArrayList<Integer> sumArrayList = new ArrayList<Integer>(newSize);
+
+    //traverse the indices of the array -- based on the size of the ListA
+    for (int i = 0; i < newSize; i++) {
+      
+      //add the Integer which is the sum from 2 given ArrayLists 
+      sumArrayList.add(ListA.get(i)+ListB.get(i));
+    }
+
+    return sumArrayList;
   }
 
   /** zipLists
@@ -104,32 +219,5 @@ public class AlPractice{
     return null;//placeholder to compile.
   }
 
-
-
-
-  public static void main(String[] args) {
-
-    ArrayList<Integer> al;
-
-    // Uncomment these to test buildRandomList
-    //al = buildRandomList(10,100);
-    //System.out.println(al);
-
-    // Uncomment these to test swapElements
-    // swapElements(2,6);
-    //System.out.println(al);
-
-    // Uncomment these to test removeValue
-    // al.add(5);
-    // al.add(10);
-    // al.add(5);
-    // al.add(13);
-    // al.set(2,5);
-    // al.set(3,5);
-    // System.out.println(a);
-    // removeValue(al,5);
-    // System.out.println(a);
-
-  }
 
 }
