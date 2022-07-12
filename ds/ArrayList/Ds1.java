af9661a4e878a976d19514815e9ad6abfdcc5ffc
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-07-12 13:49:31 +0000

    ArrayList start

---
+++ ds/ArrayList/Ds1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+pubic class Ds1{
+
+  public static void main(String[] amber){
+
+    ArrayList<String> words = new ArrayList<String>();
+
+    //adding
+    words.add("fish");
+    words.add("fish2");
+    words.add("fish3");
+    words.add(1,"somethingelse");
+
+    System.out.println(words);
+    
+  }
+    
+}
