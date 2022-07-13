39c6b049eac95cb65ac81d5f44c70e1936899ee8
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-07-13 15:40:59 +0000

    Pointers Parts1-3

---
+++ ds/LinkedLists/Driver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+  public static void main(String[] args) {
+
+    //construct a new LinkedList
+    LinkedList l = new LinkedList();
+    
+    System.out.println(l);
+  }
+}
